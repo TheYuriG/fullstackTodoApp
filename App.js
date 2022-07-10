470c48d0eb06a913133842832c096afc5de7ef52
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-07-10 09:47:19 -0300

    added header

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//? Dependencies
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import ICON from 'react-native-vector-icons/MaterialIcons';
+
+//? Colors theme
+const COLORS = { primary: '#1f145c', white: '#fff' };
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+			<View style={styles.header}>
+				<Text style={styles.headerText}>To-Do List</Text>
+				<ICON name="delete" size={25} color="red" />
+			</View>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	header: {
+		padding: 20,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+	},
+	headerText: {
+		fontWeight: 'bold',
+		fontSize: 20,
+		color: COLORS.primary,
+	},
 });
