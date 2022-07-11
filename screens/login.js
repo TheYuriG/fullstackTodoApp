acb8f22c2370f432520961a1a3c43bebd629a2c0
Yuri <yuri03042@gmail.com>
2022-07-11 16:59:35 -0300

    basic UI for login

--- screens/login.js
+++ screens/login.js
@@ -1,14 +1,42 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 
 const LoginScreen = () => {
 	return (
-		<View>
-			<Text>login</Text>
-		</View>
+		<KeyboardAvoidingView style={styles.container} behavior="padding">
+			<View style={styles.inputContainer}>
+				<TextInput
+					placeholder="Email"
+					//value={''} onChangedText={(text) => ''}
+					style={styles.input}
+				></TextInput>
+				<TextInput
+					placeholder="Password"
+					//		value={''}
+					//			onChangedText={(text) => ''}
+					secureTextEntry
+					style={styles.input}
+				></TextInput>
+			</View>
+			<View>
+				<View style={styles.buttonContainer}>
+					<TouchableOpacity onPress={() => {}} style={styles.button}>
+						<Text style={[styles.buttonText, styles.buttonOutline]}>Login</Text>
+						<Text style={styles.buttonOutlineText}>Register</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
+		</KeyboardAvoidingView>
 	);
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
