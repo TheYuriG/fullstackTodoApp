610e514c056fff9ea71b58a9cbb78edb0ae8873a
Yuri <yuri03042@gmail.com>
2022-07-11 18:01:28 -0300

    styled login screen

--- screens/login.js
+++ screens/login.js
@@ -19,13 +19,13 @@ const LoginScreen = () => {
 					style={styles.input}
 				></TextInput>
 			</View>
-			<View>
-				<View style={styles.buttonContainer}>
-					<TouchableOpacity onPress={() => {}} style={styles.button}>
-						<Text style={[styles.buttonText, styles.buttonOutline]}>Login</Text>
-						<Text style={styles.buttonOutlineText}>Register</Text>
-					</TouchableOpacity>
-				</View>
+			<View style={styles.buttonContainer}>
+				<TouchableOpacity onPress={() => {}} style={styles.button}>
+					<Text style={styles.buttonText}>Login</Text>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => {}} style={[styles.button, styles.buttonOutline]}>
+					<Text style={styles.buttonOutlineText}>Register</Text>
+				</TouchableOpacity>
 			</View>
 		</KeyboardAvoidingView>
 	);
@@ -39,4 +39,33 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
+	inputContainer: { width: '80%' },
+	input: {
+		backgroundColor: 'white',
+		paddingHorizontal: 15,
+		paddingVertical: 10,
+		borderRadius: 10,
+		marginTop: 10,
+	},
+	buttonContainer: {
+		width: '60%',
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 15,
+	},
+	button: {
+		backgroundColor: '#0782F9',
+		alignItems: 'center',
+		width: '100%',
+		padding: 15,
+		borderRadius: 10,
+	},
+	buttonOutline: {
+		backgroundColor: 'white',
+		marginTop: 5,
+		borderColor: '#0782F9',
+		borderWidth: 2,
+	},
+	buttonText: { color: 'white', fontWeight: '700', fontSize: 16 },
+	buttonOutlineText: { color: '#0782F9', fontWeight: '700', fontSize: 16 },
 });
